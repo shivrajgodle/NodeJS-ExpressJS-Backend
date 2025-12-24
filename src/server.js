68236e1269a65f61d76845487cc3bb7a4450fbc1
shivrajgodle <83442544+shivrajgodle@users.noreply.github.com>
2025-12-24 15:30:49 +0530

    Done prisma Configuration

--- src/server.js
+++ src/server.js
@@ -1,28 +1,52 @@
 import express from "express";
-
+import { config } from "dotenv";
 
 //import routes
 
 import movieRoutes from './routes/MovieRoutes.js'
+import { connectDB, disconnectDB } from "./config/db.js";
+
+config()
+connectDB()
 
 const app = express();
 
 //Api Routes
 app.use("/movies",movieRoutes);
 
-
-app.get("/hello",(req,res)=>{
-    res.json({message: "hello world"})
-})
-
-
-
-
 const PORT = 5001;
 
 const server = app.listen(PORT , ()=>{
     console.log(`Server running on port ${PORT}`);
 })
+
+
+// Handle unhandled promise rejections (e.g database connection errors)
+process.on("unhandledRejection",(err)=>{
+    console.error("unhandled Rejection", err);
+    server.close(async ()=>{
+        await disconnectDB();
+        process.exit(1);
+    });
+});
+
+
+// Handle unhandled exception
+process.on("uncaughtException", async(err)=>{
+    console.error("Uncaught Exception", err);
+    await disconnectDB();
+    process.exit(1);
+});
+
+
+// Graceful shutdown
+process.on("SIGTERM",(err)=>{
+    console.error("SIGTERM received , shutting down gracefully");
+    server.close(async ()=>{
+        await disconnectDB();
+        process.exit(0);
+    });
+});
 
 
 
