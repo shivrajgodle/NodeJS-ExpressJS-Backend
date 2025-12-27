c14d00404f6035fdffb92742d4df8b8315e68667
shivrajgodle <83442544+shivrajgodle@users.noreply.github.com>
2025-12-27 21:37:43 +0530

    Added Validation for api's

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,17 +1,18 @@
 import express from "express";
 import { addToWatchlist , removeFromWatchlist, updateWatchlist} from "../controllers/watchlistController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { addToWatchlistSchema } from "../validators/watchlistValidators.js";
+import { validateRequest } from "../middleware/validateRequest.js";
 
 const router = express.Router();
 
 router.use(authMiddleware)
 
-router.post("/", addToWatchlist);
+router.post("/", validateRequest(addToWatchlistSchema), addToWatchlist);
 
 //{{baseUrl}}/watchlist/:id
 router.delete("/:id", removeFromWatchlist)
 
 router.put("/:id",updateWatchlist)
-
 
 export default router;
